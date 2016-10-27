9d4b9924e0684da1a915c986bd50017b75e27a74
trioletas <olegs.gabrusjonoks@gmail.com>
2016-10-27 20:19:54 +0300

    Port configuration for heroku

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ require('babel-register')({
 
 const app = require('./src/app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Koa application listening on port ${port}`));
