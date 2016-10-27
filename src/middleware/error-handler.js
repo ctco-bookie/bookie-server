470b773e7046ce862f75f1e8c1247a67e65f3c76
Oleg Gabrusenok <Oleg_Gabrusenok@rcomext.com>
2016-10-27 11:26:21 +0300

    Async/Await support

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,30 +1,25 @@
-const Promise = require('bluebird');
-const co = Promise.coroutine;
+const errorHandler = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (err == null) {
+      err = new Error('Null or undefined error');
+    }
+    // some errors will have .status
+    // however this is not a guarantee
+    ctx.status = err.status || 500;
+    ctx.type = 'application/json';
+    ctx.body = JSON.stringify({
+      success: false,
+      message: err.stack
+    });
 
-const errorHandler = (ctx, next) => {
-	return co(function *() {
-		try {
-			yield next();
-		} catch (err) {
-			if (err == null) {
-				err = new Error('Null or undefined error');
-			}
-			// some errors will have .status
-			// however this is not a guarantee
-			ctx.status = err.status || 500;
-			ctx.type = 'application/json';
-			ctx.body = JSON.stringify({
-				success: false,
-				message: err.stack
-			});
-
-			// since we handled this manually we'll
-			// want to delegate to the regular app
-			// level error handling as well so that
-			// centralized still functions correctly.
-			ctx.app.emit('error', err, this);
-		}
-	})();
+    // since we handled this manually we'll
+    // want to delegate to the regular app
+    // level error handling as well so that
+    // centralized still functions correctly.
+    ctx.app.emit('error', err, this);
+  }
 };
 
-module.exports = errorHandler;
+export default errorHandler;
