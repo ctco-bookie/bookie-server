8a6d62c74b4953355b9f480ed74c480eaf419e8e
trioletas <olegs.gabrusjonoks@gmail.com>
2016-10-31 13:16:51 +0200

    Reverted Axios to request due to proxy issue (https://github.com/mzabriskie/axios/pull/491)

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -1,17 +1,10 @@
-import {delay}  from 'bluebird';
-import axios from 'axios';
+import {promisify}  from 'bluebird';
+import request from 'request';
 import CalendarDetails from './calendar-details';
 
 export async function get(ctx) {
   const calendarName = ctx.params.email;
-  const ical = await axios.get(process.env.CALENDAR_HOST.replace('${calendarName}', calendarName), {
-    // proxy: {
-    //   host: 'gate-zrh.swissre.com',
-    //   port: 8080
-    // }
-  });
-  // ctx.body = ical.data;
-
+  const ical = await promisify(request)(process.env.CALENDAR_HOST.replace('${calendarName}', calendarName));
   ctx.body = new CalendarDetails(getCalendarName(calendarName, ical.data), isBusy(calendarName, ical.data));
 }
 
