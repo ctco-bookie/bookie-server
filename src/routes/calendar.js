8d3265cc6ea82fda9854289d08a11b37f8bb6b62
Sergey Trasko <sergey.trasko@ctco.lv>
2016-11-01 11:58:12 +0200

    Reoccurring meetings

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -9,10 +9,19 @@ export const get = async ctx => {
   const calendarName = ctx.params.email;
 
   const data = await promisify(ical.fromURL)(ctx.env.CALENDAR_HOST.replace('${calendarName}', calendarName), {});
-  const events = _.map(_.filter(data, todayEvent(moment())), createEvent);
+  const events = findTodaysEvents(moment())(data);
 
   const details = new CalendarDetails(getCalendarName(calendarName), isBusy(events), events);
   ctx.body = details;
+};
+
+export const findTodaysEvents = now => {
+  return data => {
+    const events = _.filter(data, isEvent);
+    const allEvents = _.flatten(_.map(events, handleReoccurringEvent(now)));
+    const todayEvents = _.filter(allEvents, todayEvent(now));
+    return _.sortBy(_.map(todayEvents, createEvent), 'start');
+  };
 };
 
 export const getCalendarName = email => {
@@ -22,13 +31,34 @@ export const getCalendarName = email => {
   return roomName;
 };
 
+export const isEvent = event => event.type === 'VEVENT';
+
+export const handleReoccurringEvent = now => {
+  return event => {
+    if (!event.rrule) {
+      //single instance
+      return [event];
+    } else {
+      //reoccurring
+      const duration = moment.duration(moment(event.end).diff(moment(event.start)));
+
+      return _.map(event.rrule.between(now.startOf('day').toDate(), now.endOf('day').toDate()), occurrence => {
+        const start = moment(occurrence);
+        const end = moment(start).add(duration);
+        console.log(occurrence, start.toDate(), end.toDate());
+        return Object.assign({}, event, {start: start.toDate(), end: end.toDate(), duration: duration.humanize()})
+      });
+    }
+  };
+};
+
 const isBusy = events => {
   const now = moment();
   return !!_.find(events, e => now.isBetween(moment(e.start), moment(e.end)));
 };
 
 export const todayEvent = today => {
-  return (event) => {
+  return event => {
     if (!(event.start && event.end)) {
       return false;
     }
@@ -41,10 +71,10 @@ function createEvent(event) {
   e.summary = event.summary;
   e.start = event.start;
   e.end = event.end;
-  e.oranizer = {
+  e.isPrivate = !event.summary;
+  e.oranizer = event.organizer && {
     name: event.organizer.params.CN,
     email: event.organizer.val
   };
-  e.isPrivate = !event.summary;
   return e;
 }
--- src/routes/calendar.test.js
+++ src/routes/calendar.test.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import moment from 'moment';
-import {getCalendarName, todayEvent} from './calendar';
+import ical from 'ical';
+import _ from 'lodash';
+import {getCalendarName, todayEvent, findTodaysEvents} from './calendar';
 import CalendarEvent from './calendar-event';
 
 test('getCalendarName:should parse the name of the calendar', t => {
@@ -21,4 +23,64 @@ test('todayEvent:should check for today\'s events', t => {
 
   t.is(todayEvent(now)(e1), false);
   t.is(todayEvent(now)(e2), true);
+});
+
+test('parse recurrent meeting', t => {
+  const meeting =
+`BEGIN:VCALENDAR
+PRODID:Calendar-Provider
+VERSION:2.0
+METHOD:PUBLISH
+BEGIN:VTIMEZONE
+TZID:Europe/Helsinki
+BEGIN:STANDARD
+DTSTART:16010101T040000
+TZOFFSETTO:+0200
+TZOFFSETFROM:+0300
+RRULE:FREQ=YEARLY;WKST=MO;INTERVAL=1;BYMONTH=10;BYDAY=-1SU
+TZNAME:EET
+END:STANDARD
+BEGIN:DAYLIGHT
+DTSTART:16010101T030000
+TZOFFSETTO:+0300
+TZOFFSETFROM:+0200
+RRULE:FREQ=YEARLY;WKST=MO;INTERVAL=1;BYMONTH=3;BYDAY=-1SU
+TZNAME:EEST
+END:DAYLIGHT
+END:VTIMEZONE
+BEGIN:VEVENT
+UID:040000008200E00074C5B7101A82E0080000000090DF981BA0D6D1010000000000000000
+ 100000008B658AA2E062074794D229C97F3373DE
+RRULE:FREQ=WEEKLY;UNTIL=20161231T074000Z;INTERVAL=1;BYDAY=MO,TU,WE,TH,FR
+SUMMARY:Standup
+DESCRIPTION:\n\n
+LOCATION:Meeting room A
+PRIORITY:5
+X-MICROSOFT-CDO-APPT-SEQUENCE:0
+X-MICROSOFT-CDO-OWNERAPPTID:998602720
+X-MICROSOFT-CDO-BUSYSTATUS:TENTATIVE
+X-MICROSOFT-CDO-IMPORTANCE:1
+X-MICROSOFT-CDO-INSTTYPE:1
+ORGANIZER;CN=John Smith:mailto:john.smith@abc.com
+DTSTART;TZID="Europe/Helsinki":20160705T094000
+DTEND;TZID="Europe/Helsinki":20160705T100000
+STATUS:CONFIRMED
+CLASS:PUBLIC
+X-MICROSOFT-CDO-INTENDEDSTATUS:BUSY
+TRANSP:OPAQUE
+LAST-MODIFIED:20160705T063211Z
+DTSTAMP:20160705T063211Z
+SEQUENCE:0
+EXDATE;TZID="Europe/Helsinki":20160706T094000
+EXDATE;TZID="Europe/Helsinki":20160914T094000
+EXDATE;TZID="Europe/Helsinki":20161004T094000
+EXDATE;TZID="Europe/Helsinki":20161005T094000
+EXDATE;TZID="Europe/Helsinki":20161026T094000
+END:VEVENT
+END:VCALENDAR`;
+
+  const ics = ical.parseICS(meeting);
+  const events = findTodaysEvents(moment('2016-09-01'))(ics);
+  t.is(events.length, 1);
+  t.true(moment(events[0].end).isAfter(moment(events[0].start)));
 });
