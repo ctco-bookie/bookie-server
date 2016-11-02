0d89a89a91ccf80e148d7c8b93b63658e0aa4824
trioletas <olegs.gabrusjonoks@gmailcom>
2016-11-02 12:51:43 +0200

    Added possibility to search calendars by either room number or room email address

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -3,10 +3,12 @@ import _ from 'lodash';
 const rooms = JSON.parse(process.env.ROOMS ? process.env.ROOMS : '[]');
 
 const byEmail = email => _.find(rooms, room => room.email.toLowerCase() === email.toLowerCase());
+const byNumber = number => _.find(rooms, room => room.number === number);
 const byFloor = floor => _.filter(rooms, room => room.floor === floor);
 
 export default {
   byEmail,
   byFloor,
+  byNumber,
   all: () => rooms
 }
