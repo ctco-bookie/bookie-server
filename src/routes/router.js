78918b2859b3e4414e6f4c3170cb91a0b652b5d3
Sergey Trasko <sergey.trasko@ctco.lv>
2016-11-03 12:20:59 +0200

    Dropped old RESTful API

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,16 +1,8 @@
 import koaRouter from 'koa-router';
-import {
-  get as getCalendar,
-  book as book,
-  getAll as getCalendars
-} from './room-availibility.js';
 import graphQl from './graphql.js';
 
 const router = koaRouter();
 
-router.get('/calendar/:roomId', getCalendar);
-router.get('/calendars/:roomId', getCalendars);
-router.put('/calendar/:id/booking', book);
 router.all('/graphql', graphQl);
 
 export default router;
