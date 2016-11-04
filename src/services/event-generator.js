243fcfd14463f8ad63b82965f998d31e486b329f
Sergey Trasko <sergey.trasko@ctco.lv>
2016-11-04 11:52:33 +0200

    Send meeting request to organizer as well so it can be cancelled if needed

--- src/services/event-generator.js
+++ src/services/event-generator.js
@@ -1,17 +1,23 @@
 import ical from 'ical-generator';
 
-export const createICal = ({start, end, organizerName, organizerEmail}) => {
+export const createICal = ({start, end, organizerName, organizerEmail, calendarEmail}) => {
   const cal = ical({
     events: [
       {
         start: start.toDate(),
         end: end.toDate(),
         summary: 'Ad-hoc',
-        method: 'REQUEST',
+        method: 'request',
         organizer: {
           name: organizerName,
           email: organizerEmail
-        }
+        },
+        attendees: [
+          {
+            name: calendarEmail,
+            email: calendarEmail
+          }
+        ]
       }
     ]
   });
